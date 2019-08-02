f0655758cf846c9f1cc9ea043c798e3cdef3d223
imgomez0127 <ianm0127@hotmail.com>
2019-08-02 11:35:26 -0400

    Added another question was rlly ez can optimize with bin search

---
+++ interview-questions/dip5.py
@@ -0,0 +1,16 @@
+class Solution: 
+    def getRange(self, arr, target):
+        beginning = -1
+        found_first_item = False
+        for i,n in enumerate(arr):
+            if n == target and not found_first_item:
+                beginning = i
+                found_first_item = True
+            if n != target and found_first_item:
+                return beginning,i-1
+        return beginning,-1
+# Test program 
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
+x = 2
+print(Solution().getRange(arr, x))
+# [1, 4]
