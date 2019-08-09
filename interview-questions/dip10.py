60522b9e07f4a2663f4100c024ea08773f0c7799
imgomez0127 <ianm0127@hotmail.com>
2019-08-09 14:18:13 -0400

    Added another question invert bintree

---
+++ interview-questions/dip10.py
@@ -0,0 +1,31 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+  def preorder(self):
+    print(self.value)
+    if self.left: self.left.preorder()
+    if self.right: self.right.preorder()
+
+def invert(node):
+    if node == None:
+        return None
+    node_temp = node.left
+    node.left = invert(node.right)
+    node.right = invert(node_temp)
+    return node
+
+root = Node('a') 
+root.left = Node('b') 
+root.right = Node('c') 
+root.left.left = Node('d') 
+root.left.right = Node('e') 
+root.right.left = Node('f') 
+
+root.preorder()
+# a b d e c f 
+print("\n")
+invert(root)
+root.preorder()
+# a c f b e d
