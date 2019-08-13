59fc7169a5e85fc2642cfaebb945b8aadfba53e7
imgomez0127 <ianm0127@hotmail.com>
2019-08-13 09:44:59 -0400

    Added a daily interview pro question slow solution

---
+++ interview-questions/dip13.py
@@ -0,0 +1,15 @@
+def f(lst):
+  for i in range(len(lst)):
+    dcopy = {}
+    for num in lst:
+      dcopy[num] = dcopy.get(num,0) + 1
+    dcopy[lst[i]] -= 1
+    for j in range(i,len(lst)):
+      dcopy[lst[j]]-= 1
+      last_triple = ((lst[i])**2 + (lst[j])**2)**0.5
+      if dcopy.get(last_triple,0) > 0:
+        return True
+  return False
+print(f([3,4,5]))
+print(f([3,5,12,13]))
+print(f([1,1,1]))
