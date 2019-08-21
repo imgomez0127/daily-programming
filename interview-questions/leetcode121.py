2c7f1d5f46c0cd02418d5555b9f6196375203e43
imgomez0127 <ianm0127@hotmail.com>
2019-08-21 11:26:19 -0400

    Added a more efficient solution

--- interview-questions/leetcode121.py
+++ interview-questions/leetcode121.py
@@ -1,11 +1,14 @@
-def maxProfit(self, prices):
-      """
-      :type prices: List[int]
-      :rtype: int
-      """
-      max_profit = 0
-      for i in range(len(prices)):
-          cur_profit = -prices[i]
-          for j in range(i,len(prices)):
-              max_profit = max(max_profit,cur_profit+prices[j])
-      return max_profit
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if prices == []:
+            return 0
+        max_profit = 0
+        min_price = prices[0]
+        for i in  range(1,len(prices)):
+            max_profit = max(max_profit,prices[i]-min_price)
+            min_price = min(min_price,prices[i])
+        return max_profit
