981d63fc7504cff108af1f4476b34cc6d2a071a4
imgomez0127 <ianm0127@hotmail.com>
2019-09-03 14:41:09 -0400

    Added more efficient solution

--- interview-questions/leetcode5.py
+++ interview-questions/leetcode5.py
@@ -1,3 +1,23 @@
+import string
 class Solution:
-    def longestPalindrome(self, s):
-        pass
+    def find_new_starting_char(self,s,c):
+        for i,char in enumerate(s):
+            if char == c:
+                return i+1
+        return -1
+    
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+        max_len = 1
+        starting_char = 0
+        seen_chars = {char: -1 for char in string.printable}
+        seen_chars[s[0]] = 0
+        for i in range(1,len(s)):
+            if seen_chars[s[i]] != -1:
+                starting_char = max(seen_chars[s[i]] + 1,len(s)-1)
+            max_len = max(max_len,i-starting_char+1)
+            seen_chars[s[i]] = i
+        return max_len
+
+print(Solution().lengthOfLongestSubstring("abba"))
