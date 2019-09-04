175977792c0d159c95a357dcf8c5c22dcbf42110
imgomez0127 <ianm0127@hotmail.com>
2019-09-04 14:48:02 -0400

    Added a slightly more efficient DP solution where answers are cached

--- interview-questions/leetcode55.py
+++ interview-questions/leetcode55.py
@@ -1,17 +1,23 @@
 class Solution:
-    def canJumpHelper(self,nums,position):
-        if position >= len(nums)-1:
-            return True
-        for jump_amount in range(1,nums[position]+1):
-            if self.canJumpHelper(nums,position+jump_amount):
+    def canJumpHelper(self,nums,position,able_to_reach_end):
+        if able_to_reach_end[position] != 0:
+            return True if able_to_reach_end[position] == 1 else False
+        else:
+            if position >= len(nums)-1:
                 return True
-        return False
+            for jump_amount in range(1,nums[position]+1):
+                if self.canJumpHelper(nums,position+jump_amount,able_to_reach_end):
+                    able_to_reach_end[position] = 1
+                    return True
+            able_to_reach_end[position] = -1
+            return False
 
     def canJump(self, nums):
+        able_to_reach_end = [0 for _ in range(len(nums))]
         if len(nums) == 1:
             return True
         for jump_amount in range(1,nums[0]+1):
-            if self.canJumpHelper(nums,jump_amount):
+            if self.canJumpHelper(nums,jump_amount,able_to_reach_end):
                 return True
         return False
 
