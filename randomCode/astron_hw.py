d525c6796acdc753c41bd64ca1b59c98d5b51be2
imgomez0127 <ianm0127@hotmail.com>
2019-10-09 10:38:55 -0400

    Added stuff

--- randomCode/astron_hw.py
+++ randomCode/astron_hw.py
@@ -6,11 +6,22 @@ uranus_moons = pd.read_csv("uranus_moons.csv")
 semi_major_axis,orbital_period = np.abs(np.asarray(uranus_moons["Semi-major axis"],dtype=np.float64)),np.abs(np.asarray(uranus_moons["Orbital period"],dtype=np.float64))
 semi_major_axis = semi_major_axis * 1000
 orbital_period = orbital_period * 24 * 60 * 60
+uranus_moons["Semi major axis in meters"] = semi_major_axis
+uranus_moons["Orbital period in seconds"] = orbital_period
 semi_major_axis = semi_major_axis**3
 orbital_period = np.power(orbital_period,2)
+uranus_moons["Semi major axis in meters squared"] = semi_major_axis
+uranus_moons["Orbital period in seconds cubed"] = orbital_period
 print(semi_major_axis)
 print(orbital_period)
 X = np.hstack((np.ones((semi_major_axis.shape[0],1)),semi_major_axis[np.newaxis].T))
 print(semi_major_axis.dot(orbital_period)-(semi_major_axis.shape[0]*(semi_major_axis.dot(orbital_period))))
 print(sum(np.power(semi_major_axis,2)))
-print(np.linalg.inv(X.T.dot(X)).dot(X.T).dot(orbital_period))
+theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(orbital_period)
+print(theta)
+print(uranus_moons)
+plt.scatter(semi_major_axis,orbital_period)
+plt.plot(semi_major_axis,X.dot(theta))
+plt.ylabel("Period Cubed")
+plt.xlabel("Semi Major Axis Square")
+plt.show()
