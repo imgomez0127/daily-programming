07f8fff9ea4f4801e4380c6e48474c7ecd897506
imgomez0127 <ianm0127@gmail.com>
2020-12-25 18:17:21 -0500

    yes

--- algorithm-implementations/trie.ml
+++ algorithm-implementations/trie.ml
@@ -14,7 +14,7 @@ let explode (str : string) : char list =
   let rec explode_inner cur_index chars =
     if cur_index < String.length str then
       let new_char = str.[cur_index] in
-      explode_inner (cur_index + 1) (chars @ [new_char])
+      explode_inner (cur_index+1) (chars@[new_char])
     else chars in
   explode_inner 0 []
 
