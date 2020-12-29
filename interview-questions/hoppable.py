863b347879b7d9d8aca33115662ea732792bc368
imgomez0127 <ianm0127@gmail.com>
2020-12-28 20:46:27 -0500

    optimized question

--- interview-questions/hoppable.py
+++ interview-questions/hoppable.py
@@ -4,13 +4,8 @@ class Solution:
     def solve(self, nums):
         if not nums:
             return None
-        solvable = [False] * len(nums)
-        exited = False
-        for i, num in enumerate(nums):
-            exited |= num >= len(nums)-i-1
-            solvable[i] = exited
-        last_true = len(nums)-1
-        for i in reversed(range(len(nums))):
-            solvable[i] |= i+nums[i] >= last_true
-            last_true = i if solvable[i] else last_true
-        return solvable[0]
+        reach = 0
+        for i, n in enumerate(nums):
+            if i > reach:
+                return False
+        return True
