ebe0e985eb92ec819684da0fce76c39058bf2b46
imgomez0127 <ianm0127@gmail.com>
2021-11-18 10:33:40 -0500

    Reformatted code a bit

--- algorithm-implementations/gaussian_mixture_models.py
+++ algorithm-implementations/gaussian_mixture_models.py
@@ -2,23 +2,23 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 
+
 def preprocess_data(data):
     return data
 
-def norm(V):
-    return sum(V@V.T)
 
 class GMM:
+
     def __init__(self, feature_size, num_classes=3):
         self.means = np.random.random((feature_size, num_classes))
-        self.covs = np.array([np.identity(feature_size) for _ in range(num_classes)])
+        self.covs = np.array([np.identity(feature_size)
+                              for _ in range(num_classes)])
         self.num_classes = num_classes
         self.pis = np.random.random((1, num_classes))
 
     def expectation_step(self, iris_data):
         gammas = []
-        for i in range(len(iris_data)):
-            sample = iris_data[i]
+        for sample in iris_data:
             normalization_term = 0
             sample_gammas = np.zeros((self.num_classes))
             for j in range(self.num_classes):
@@ -38,8 +38,7 @@ class GMM:
         means = np.zeros(self.means.shape)
         covs = np.zeros(self.covs.shape)
         pis = assignment_counts/len(iris_data)
-        for i in range(len(iris_data)):
-            sample = iris_data[i]
+        for i, sample in enumerate(iris_data):
             for j in range(len(gammas)):
                 means[j] += gammas[i][j]*sample
                 std = (sample-self.means[j])
@@ -47,7 +46,6 @@ class GMM:
         means = means/assignment_counts[:, None]
         covs = covs/assignment_counts[:, None]
         return means, covs, pis
-
 
     def train(self, iris_data, epochs=100):
         for _ in range(epochs):
@@ -59,6 +57,6 @@ class GMM:
 
 
 def main():
-    iris_data = pd.read_csv('iris.data')
+    iris_data = preprocess_data(pd.read_csv('iris.data'))
     GMM_MODEL = GMM(iris_data.shape[1])
     GMM_MODEL.train(iris_data)
